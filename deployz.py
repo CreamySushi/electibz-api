8e15b549bb077b4c41d0c1ed4fdde52df8b15a02
tian403 <morando.markchristian@gmail.com>
2025-04-18 23:03:43 +0800

    Update deployz.py

--- deployz.py
+++ deployz.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from fastapi import FastAPI
 import joblib
 import numpy as np
@@ -17,23 +16,3 @@ def predict(features: list):
     features = np.array(features).reshape(1, -1)
     prediction = model.predict(features)
     return {"Predicted Calories": prediction.tolist()}
-=======
-from fastapi import FastAPI
-import joblib
-import numpy as np
-
-app = FastAPI()
-
-# Load the saved model
-model = joblib.load("xgb_model.pkl")
-
-@app.get("/")
-def home():
-    return {"message": "Calorie Prediction API is running!"}
-
-@app.post("/predict/")
-def predict(features: list):
-    features = np.array(features).reshape(1, -1)
-    prediction = model.predict(features)
-    return {"Predicted Calories": prediction.tolist()}
->>>>>>> 6917c7d77a4d0b06569cb69ffe42aa06f6975c73
