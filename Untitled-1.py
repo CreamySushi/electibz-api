33f65587e320cb6918ca3576cfcccd734e11aab6
tian403 <morando.markchristian@gmail.com>
2025-04-18 21:55:44 +0800

    Delete Untitled-1.py

--- Untitled-1.py
+++
@@ -1,5 +0,0 @@
-try:
-    from xgboost import XGBRegressor
-    print("XGBRegressor is available.")
-except ImportError:
-    print("XGBRegressor is not available. XGBoost may not be installed correctly.")
