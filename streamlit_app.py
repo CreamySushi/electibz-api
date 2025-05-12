d9c02e51687b5c1681f80203704d30fefd452aed
CreamySushi <152081964+CreamySushi@users.noreply.github.com>
2025-05-13 00:32:43 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,8 @@ if "email" not in st.session_state:
     st.session_state.email = None
 if "admin" not in st.session_state:
     st.session_state.admin = False
+if "about_us" not in st.session_state:
+    st.session_state.about_us = False
 
 background_image = """
 <style>
@@ -468,19 +470,20 @@ def Show_Main_Screen():
         else:
             st.warning("Could not generate graph due to API errors.")
 
-def Show_About_Us():
+def Show_About_Us_Screen():
     st.title("About Us")
     st.markdown("""
         Welcome to CALORIE BURN PREDICTOR — where smart tech meets your fitness goals!
-We know that juggling classes, assignments, and social life can make staying healthy feel like an uphill battle. That’s why we’re here to make fitness easier, smarter, and way more personal.
-Let’s face it — tracking calories and managing weight isn’t always straightforward. Most apps just toss out random estimates that don’t really fit you. That’s where we come in. Our platform uses powerful machine learning (don’t worry, we made it simple!) to give you accurate calorie burn predictions based on your unique stats — like age, height, weight, gender, and the type of activity you’re doing.
-Behind the scenes, our tech runs on the XGBoost algorithm (basically, one of the smartest ways to crunch numbers fast), so you get real-time, precise info to help you manage your fitness and hit your goals — whether it’s getting stronger, staying in shape, or just feeling good.
-This project was proudly developed by Third Year Students, Computer Engineering -(CPE3A) from the University of Perpetual Help System,Laguna as a practical application to help users track their calorie burn. By simply entering a few key personal details, our app accurately calculates the calories you burn during activities. Using advanced machine learning — specifically the XGBoost algorithm — we provide precise, individualized results to help you manage your fitness more effectively.
-Our mission?
-To give you a tool that actually works for you — no guesswork, no gimmicks — just clear insights so you can take control of your health with confidence.
-Stay sharp. Stay fit. Let’s do this together at CALORIE BURN PREDICTOR.
-        
-    """)
+        We know that juggling classes, assignments, and social life can make staying healthy feel like an uphill battle. That’s why we’re here to make fitness easier, smarter, and way more personal.
+        Let’s face it — tracking calories and managing weight isn’t always straightforward. Most apps just toss out random estimates that don’t really fit you. That’s where we come in. Our platform uses powerful machine learning (don’t worry, we made it simple!) to give you accurate calorie burn predictions based on your unique stats — like age, height, weight, gender, and the type of activity you’re doing.
+        Behind the scenes, our tech runs on the XGBoost algorithm (basically, one of the smartest ways to crunch numbers fast), so you get real-time, precise info to help you manage your fitness and hit your goals — whether it’s getting stronger, staying in shape, or just feeling good.
+        This project was proudly developed by Third Year Students, Computer Engineering -(CPE3A) from the University of Perpetual Help System,Laguna as a practical application to help users track their calorie burn. By simply entering a few key personal details, our app accurately calculates the calories you burn during activities. Using advanced machine learning — specifically the XGBoost algorithm — we provide precise, individualized results to help you manage your fitness more effectively.
+        Our mission?
+        To give you a tool that actually works for you — no guesswork, no gimmicks — just clear insights so you can take control of your health with confidence.
+        Stay sharp. Stay fit. Let’s do this together at CALORIE BURN PREDICTOR.
+                
+    """,unsafe_allow_html=True)
+    st.rerun()
 
 
 
@@ -509,7 +512,7 @@ else:
             
             st.markdown("<br><br><br><br><br><br>", unsafe_allow_html=True)
             if st.button("About Us"):
-                Show_About_Us()
+                Show_About_Us_Screen()
             if st.button("Contact Us"):
                 
                 st.markdown("""
@@ -531,7 +534,7 @@ else:
 
         else:
             if st.button("About Us"):
-                Show_About_Us()
+                Show_About_Us_Screen()
             if st.button("Contact Us"):
                 
                 st.markdown("""
@@ -557,6 +560,8 @@ else:
         Show_Forgot_Password_Screen()
     elif st.session_state.show_signup:
         Show_Sign_Up_Screen()
+    elif st.session_state.about_us:
+        Show_About_Us_Screen()
     else:
         Show_Login_Screen()
 
