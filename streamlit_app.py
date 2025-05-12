6092046d49b91204cde168b8ec7d5d6ff9bc02da
CreamySushi <152081964+CreamySushi@users.noreply.github.com>
2025-05-12 23:21:48 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -358,7 +358,7 @@ def Show_Main_Screen():
                 "Username", "Gender", "Age", "Height (cm)", "Weight (kg)",
                 "Duration (min)", "Heart Rate", "Body Temp (°C)", "Calories Burned"
             ])
-            st.dataframe(df, use_container_width=True, disabled=True)
+            st.data_editor(df, use_container_width=True, disabled=True)
         else:
             st.info("No prediction history available.")
         return
