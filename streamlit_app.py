b70bfe076eed524769ccc6dd294c97e08a709e4f
CreamySushi <152081964+CreamySushi@users.noreply.github.com>
2025-05-13 00:15:36 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -467,6 +467,23 @@ def Show_Main_Screen():
             st.pyplot(fig)
         else:
             st.warning("Could not generate graph due to API errors.")
+
+def Show_About_Us():
+    st.title("About Us")
+    st.markdown("""
+        Welcome to CALORIE BURN PREDICTOR — where smart tech meets your fitness goals!
+We know that juggling classes, assignments, and social life can make staying healthy feel like an uphill battle. That’s why we’re here to make fitness easier, smarter, and way more personal.
+Let’s face it — tracking calories and managing weight isn’t always straightforward. Most apps just toss out random estimates that don’t really fit you. That’s where we come in. Our platform uses powerful machine learning (don’t worry, we made it simple!) to give you accurate calorie burn predictions based on your unique stats — like age, height, weight, gender, and the type of activity you’re doing.
+Behind the scenes, our tech runs on the XGBoost algorithm (basically, one of the smartest ways to crunch numbers fast), so you get real-time, precise info to help you manage your fitness and hit your goals — whether it’s getting stronger, staying in shape, or just feeling good.
+This project was proudly developed by Third Year Students, Computer Engineering -(CPE3A) from the University of Perpetual Help System,Laguna as a practical application to help users track their calorie burn. By simply entering a few key personal details, our app accurately calculates the calories you burn during activities. Using advanced machine learning — specifically the XGBoost algorithm — we provide precise, individualized results to help you manage your fitness more effectively.
+Our mission?
+To give you a tool that actually works for you — no guesswork, no gimmicks — just clear insights so you can take control of your health with confidence.
+Stay sharp. Stay fit. Let’s do this together at CALORIE BURN PREDICTOR.
+        
+    """)
+
+
+
 initialize_database()
 # Main navigation flow
 if not st.session_state.splash_shown:
@@ -478,6 +495,8 @@ else:
         if st.session_state.logged_in:
             st.button(f"Welcome, {st.session_state.username}")
             st.markdown("---")
+            if st.button("Home"):
+                Show_Main_Screen()
             if st.button("Refresh"):
                 st.rerun()
             st.markdown("<br><br><br>", unsafe_allow_html=True)
@@ -490,6 +509,7 @@ else:
             
             st.markdown("<br><br><br><br><br><br>", unsafe_allow_html=True)
             if st.button("About Us"):
+                Show_About_Us()
                 st.write("This is a project made by college students of University of Perpetual Help System [Laguna] as an output application to track the calorie burn of the user by input some necessary details to calculate the calories. This application uses a machine learning algorithm to calculate calories individually using xgb algorithm.")
             
             if st.button("Contact Us"):
